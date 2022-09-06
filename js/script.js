a8a34eb28efb80a1ad255b1cbab20cbe93ae9f42
Amine <ouas.amine@outlook.com>
2022-09-06 10:48:49 +0100

    Fix JS Script Directory

--- js/script.js
+++ js/script.js
@@ -3,16 +3,16 @@ const closeNav = document.querySelector('#close-navlist');
 const navList = document.querySelector('#nav-list');
 const navListItems = document.querySelectorAll('#nav-list > ul > li');
 navBtn.addEventListener('click', () => {
-    navList.classList.remove('hide');
-    navList.classList.add('mobile-menu');
+  navList.classList.remove('hide');
+  navList.classList.add('mobile-menu');
 });
 
 closeNav.addEventListener('click', () => {
-    navList.classList.add('hide');
-    navList.classList.remove('mobile-menu');
+  navList.classList.add('hide');
+  navList.classList.remove('mobile-menu');
 });
 
 navListItems.forEach((item) => item.addEventListener('click', () => {
-    navList.classList.add('hide');
-    navList.classList.remove('mobile-menu');
+  navList.classList.add('hide');
+  navList.classList.remove('mobile-menu');
 }));
